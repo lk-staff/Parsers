768fbc37c371160872439c9517cc58fefc0ea4fc
Alexander Buzmakov <97955733+Boozy044@users.noreply.github.com>
2022-12-24 23:46:18 +0300

    Feature/project-setup (#3)
    * chore: project setup
    
    add layouts
    add tailwind css
    add google fonts
    add fontawesome

---
+++ packages/renderer/src/pages/IndexPage.vue
@@ -0,0 +1,11 @@
+<template>
+  <div> Index Page </div>
+  <v-icon
+    icon="fa-solid fa-user-secret"
+    size="6x"
+  />
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped></style>
